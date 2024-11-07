a2c71cd241a3172f24a91d1bd9422d5c0f3f39cb
BhavaniPenugonda <bhavani0202@gmail.com>
2024-11-07 15:16:43 +0530

    events are listed in json object

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -105,7 +105,7 @@ module.exports.getCalendarEvents = async (event) => {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
-    body: JSON.stringify(results),
+    body: JSON.stringify({ events: results.data.items }),
   };
 })
 .catch((error) => {
