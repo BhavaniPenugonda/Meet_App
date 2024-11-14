b6a31d3ae1dea22f7d2da78af22dd06a42665efe
BhavaniPenugonda <bhavani0202@gmail.com>
2024-11-14 16:27:41 +0530

    test is added to check the entered city

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <EventList/></div>
+      <EventList/>
+      <CitySearch/>
+      </div>
     
   );
 }
