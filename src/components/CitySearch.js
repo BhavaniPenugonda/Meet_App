4f5ad279aa957299ff2f31c51f84205cc032ab48
BhavaniPenugonda <bhavani0202@gmail.com>
2024-11-14 21:58:52 +0530

    onClick is added

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -31,9 +31,9 @@ const CitySearch = ({ allLocations }) => {
       />
       {showSuggestions ? <ul className="suggestions">
         {suggestions.map((suggestion) => {
-            return <li key={suggestion}>{suggestion}</li>
+            return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
           })}
-          <li key='See all cities'>
+          <li key='See all cities' onClick={handleItemClicked}>
             <b>See all cities</b>
           </li>
       </ul> : null}
