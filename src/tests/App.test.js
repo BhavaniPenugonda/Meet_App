78118ac52b58eaafc18974964ba3208698bf3bd7
BhavaniPenugonda <bhavani0202@gmail.com>
2024-11-15 20:39:40 +0530

    AppDOM is replaced with screen

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,38 +1,38 @@
-import { render ,within} from '@testing-library/react';
+import {render ,within,screen} from '@testing-library/react';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
 
 
 
 describe('<App /> component', () => {
-  let AppDOM;
+
   beforeEach(() => {
-    AppDOM = render(<App />).container.firstChild;
+    render(<App />);
   })
   test('renders list of events', () => {
      
-    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+    expect(screen.getByTestId('#event-list')).toBeInTheDocument();
   });
   test('render CitySearch', () => {
     
-    expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+    expect(screen.getByTestId('#city-search')).toBeInTheDocument();
     });
     test('render number of events', () => {
-      expect(AppDOM.querySelector('#numberOfEvents')).toBeInTheDocument();
+      expect(screen.getByTestId('#numberOfEvents')).toBeInTheDocument();
       });
 });
 
 test('updates the number of events displayed when the user changes the number of events input', async () => {
   const user = userEvent.setup();
-  const AppComponent = render(<App />);
-  const AppDOM = AppComponent.container.firstChild;
+  render(<App />);
+  
 
-  const NumberOfEventsDOM = AppDOM.querySelector('#number-of-events');
+  const NumberOfEventsDOM =screen.getByTestId ('#number-of-events');
   const NumberOfEventsInput = within(NumberOfEventsDOM).queryByTestId('numberOfEventsInput');
 
   await user.type(NumberOfEventsInput, '{backspace}{backspace}10');
 
-  const EventListDOM = AppDOM.querySelector('#event-list');
+  const EventListDOM = screen.getByTestId('#event-list');
   const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');
   expect(allRenderedEventItems.length).toBe(10);
 });
