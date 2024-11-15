b4ce6237c3961eddbab5ab40b3c5633922725a66
BhavaniPenugonda <bhavani0202@gmail.com>
2024-11-15 13:00:20 +0530

    onClick is handeled

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,23 @@
-const Event = () => {
+import { useState } from "react";
+
+const Event = ({event}) => {
+
+  const [showDetails, setShowDetails] = useState(false);
   return (
-    <li></li>
+    <li
+      className="event">
+            <p>{event.summary}</p>
+           
+            <p>{event.start?.dateTime}</p>
+            <p>{event.end?.dateTime}</p>
+            <p>{event.location}</p>
+            <button className="detail-btn" onClick={()=>setShowDetails(!showDetails)}>
+                { showDetails ? "Hide details" : "Show details"}
+            </button>
+            {
+                showDetails ?  <p role="description" className="details">{event.description}</p> : <></>
+            }
+    </li>
   );
 }
 
