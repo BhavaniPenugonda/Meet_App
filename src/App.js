2bea459cc697cedfb0e88897f9de4bb554074b64
BhavaniPenugonda <bhavani0202@gmail.com>
2024-11-16 19:19:10 +0530

    events and currentnoe states are created

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import Event from './components/Event';
 import CitySearch from './components/CitySearch';
 import './App.css';
 import NumberOfEvents from './components/NumberOfEvents';
+import { useEffect, useState } from 'react';
+import { getEvents } from './api';
+
+const App = () => {
+
+const [events, setEvents] = useState([]);
+const [currentNOE, setCurrentNOE] = useState(32);
 
 
-function App() {
+const fetchData = async () => {
+  const allEvents = await getEvents();
+  setEvents(allEvents.slice(0, currentNOE));
+}
+
+
+useEffect(() => {
+  fetchData();
+}, []);
+
+
+
   return (
     <div className="App">
-      <EventList/>
+      <EventList events={events} />
       <CitySearch/>
       <Event/>
+      <NumberOfEvents />
     
       
       </div>
