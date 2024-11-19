41410ca29430e3b9a83a841594bd388fbfcbf31e
BhavaniPenugonda <bhavani0202@gmail.com>
2024-11-19 13:58:49 +0530

    function prop is rerendered

--- src/tests/CitySearch.test.js
+++ src/tests/CitySearch.test.js
@@ -61,7 +61,7 @@ describe('<CitySearch /> component', () => {
     const user = userEvent.setup();
     const allEvents = await getEvents(); 
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} setCurrentCity={() => { }}/>);
 
     const cityTextBox = screen.queryByRole('textbox');
     await user.type(cityTextBox, "Berlin");
