6e8a9495ef89085ee36b9b271bd789f529cd2352
BhavaniPenugonda <bhavani0202@gmail.com>
2024-11-25 18:45:57 +0100

    beforeAll() and afterAll() are declared

--- src/tests/EndToEnd.test.js
+++ src/tests/EndToEnd.test.js
@@ -2,29 +2,32 @@ import {describe } from '@jest/globals';
 import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
-  test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch();
-
-    const page = await browser.newPage();
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
+  });
 
+  afterAll(() => {
+    browser.close();
+  });
 
+  test('An event element is collapsed by default', async () => {
+    
     const eventDetails = await page.$('.event .details');
-    expect(eventDetails).toBeNull();
-    browser.close();   
+    expect(eventDetails).toBeNull(); 
 
 });
-  test('User can expand an event to see its details', async () => {
-   const browser = await puppeteer.launch();
-   const page = await browser.newPage();
-   await page.goto('http://localhost:3000/');
 
-   await page.waitForSelector('.event');
+  test('User can expand an event to see its details', async () => {
+   
    await page.click('.event .detail-btn');
 
    const eventDetails = await page.$('.event .details');
    expect(eventDetails).toBeDefined();
-   browser.close();
+
   });
 });
