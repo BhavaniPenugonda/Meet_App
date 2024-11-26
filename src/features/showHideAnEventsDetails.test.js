0283365426006279352883026464a2a3943d5387
BhavaniPenugonda <bhavani0202@gmail.com>
2024-11-26 17:59:03 +0100

    code is implemented to check the tests

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,47 +1,84 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import { render ,within,waitFor} from '@testing-library/react';
+import App from '../App';
+import { getEvents } from '../api';
+import userEvent from '@testing-library/user-event';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 
 defineFeature(feature, test => {
-
+  let AppComponent;
+  let AppDOM; 
+  let EventListDOM;
   test('An event element is collapsed by default', ({ given, when, then }) => {
+
     given('the user has opened the event list', () => {
+      AppComponent = render(<App />);
+      
+  });
 
+    when('the user views the event elements', async() => {
+      AppDOM = AppComponent.container.firstChild;
+      EventListDOM = AppDOM.querySelector('#event-list');
+      await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBeGreaterThan(0);
     });
-
-    when('the user views the event elements', () => {
-
     });
 
     then('each event element should be displayed in a collapsed state', () => {
-
+      AppDOM = AppComponent.container.firstChild;
+      const eventDetails = AppDOM.querySelector('.details');
+      expect(eventDetails).not.toBeInTheDocument();
     });
   });
-  test('User can expand an event to see details.', ({ given, when, then }) => {
-    given('the user has opened the event list', () => {
 
+  test('User can expand an event to see details.', ({ given, when, then }) => {
+    given('the user has opened the event list', async() => {
+      AppComponent = render(<App />);
+      AppDOM = AppComponent.container.firstChild;
+      EventListDOM = AppDOM.querySelector('#event-list');
+      await waitFor(() => {
+            const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+            expect(EventListItems.length).toBe(32);
+          });
     });
 
-    when('the user clicks on the expand button of the event', () => {
-
+    when('the user clicks on the expand button of the event', async() => {
+      const user = userEvent.setup();
+      const showDetails = AppComponent.queryByText('Show Details');
+      await user.click(showDetails);
     });
 
     then('the event should expand to show its details', () => {
-
+      AppDOM = AppComponent.container.firstChild;
+      const eventDetails = AppDOM.querySelector('.details');
+      expect(eventDetails).toBeInTheDocument();
     });
   });
   test('User can collapse an event to hide details', ({ given, when, then }) => {
-    given('the user has opened the event list', () => {
+    given('the user has opened the event list', async() => {
+      AppComponent = render(<App />);
+      AppDOM = AppComponent.container.firstChild;
+      EventListDOM = AppDOM.querySelector('#event-list');
+      await waitFor(() => {
+            const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+            expect(EventListItems.length).toBe(32);
+          });
 
     });
 
     when('the user clicks on the collapse button of the event', () => {
-
+      const user = userEvent.setup();
+      const hideDetails = AppComponent.queryByText('Hide Details');  
+      user.click(hideDetails); 
     });
 
     then('the event should collapse to hide its details', () => {
-
+      AppDOM = AppComponent.container.firstChild;
+      const eventDetails = AppDOM.querySelector('.details');
+      expect(eventDetails).not.toBeInTheDocument();
     });
   });
 
--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,33 +1,58 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import NumberOfEvents from "../components/NumberOfEvents";
+import App from "../App";
+import { render,within,waitFor} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import NumberOfEvents from "../components/NumberOfEvents";
 
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 
 defineFeature(feature, test => {
+  let AppComponent;
+  let EventListDOM;
+  let AppDOM;
+  let NumberOfEventsComponent;
   test('When user hasn’t specified a number, 32 events are shown by default.', ({ given, when, then }) => {
-    given('the user has opened the event list', () => {
-
+    given('the user has opened the event list', async() => {
+      AppComponent = render(<App />);
+      
+      
     });
 
     when('the user has not specified the number of events to display', () => {
-
+      
     });
 
-    then(/^the app should show (\d+) events by default$/, (arg0) => {
-
+    then(/^the app should show (\d+) events by default$/, async(arg0) => {
+      AppDOM = AppComponent.container.firstChild;
+      EventListDOM = AppDOM.querySelector('#event-list');
+      await waitFor(() => {
+            const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+            expect(EventListItems.length).toBe(32);
+          });
     });
   });
   test('User can change the number of events displayed.', ({ given, when, then }) => {
-    given('the user has opened the event list', () => {
-
+    given('the user has opened the event list', async() => {
+      AppComponent = render(<App />);
+      AppDOM = AppComponent.container.firstChild;
+      EventListDOM = AppDOM.querySelector('#event-list');
+      await waitFor(() => {
+            const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+            expect(EventListItems.length).toBe(32);
+          });
     });
 
-    when(/^the user specifies a new number of events to display \(e.g., (\d+)\)$/, (arg0) => {
-
+    when(/^the user specifies a new number of events to display \(e.g., (\d+)\)$/, async(arg0) => {
+      NumberOfEventsComponent = render(<NumberOfEvents setCurrentNOE={() => {}} setErrorAlert={() => {}} />)
+      const user = userEvent.setup();
+      const numberOfEvents = NumberOfEventsComponent.getByRole('textbox');
+      await user.type(numberOfEvents, '{backspace}{backspace}10'); 
     });
 
     then('the app should update the event list to show the specified number of events', () => {
-
+      expect(NumberOfEventsComponent.getByRole('textbox')).toHaveValue('10'); 
     });
   });
 
