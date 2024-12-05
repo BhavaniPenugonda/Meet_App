14355e461c161514f7e30f80ec1c3d0d0c29f598
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-05 14:42:00 +0100

    warning Alert component is created

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -48,4 +48,11 @@ class ErrorAlert extends Alert {
       this.bgColor = 'rgb(255, 132, 136)'; // light red
   }
 }
-export { InfoAlert,ErrorAlert};
+class WarningAlert extends Alert {
+  constructor(props) {
+      super(props);
+      this.color = "rgb(150, 174, 55)";
+      this.bgColor = "rgb(255, 255, 220)";
+  }
+}
+export { InfoAlert,ErrorAlert,WarningAlert};
