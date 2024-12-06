c4f91e228cb89f53cd5125767ca07211dc44131c
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-06 14:23:14 +0100

    indentation is applied

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -1,12 +1,5 @@
 import { useState, useEffect } from 'react';
-import {
-  ScatterChart,
-  Scatter,
-  XAxis, YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer
-} from 'recharts';
+import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const CityEventsChart = () => {
 }
