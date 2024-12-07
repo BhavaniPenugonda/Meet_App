dc74da6a6d836c7ec9edf5ec48749afadcb69369
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-07 16:06:34 +0100

    imported and rendered the component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { useEffect, useState } from 'react';
 import {extractLocations, getEvents } from './api';
 import { InfoAlert ,ErrorAlert,WarningAlert} from './components/Alert';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
+
 
 const App = () => {
 
@@ -53,6 +55,7 @@ return (
       </div>
       <div className="charts-container">
       <CityEventsChart allLocations={allLocations} events={events} />
+      <EventGenresChart events={events} />
       </div>
     <EventList events={events} />
     
